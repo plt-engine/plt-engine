6ad922e18cf4f2f01865947ad4c4f4738e4bde4d
plt-engine <walidjunior2001@gmail.com>
2026-07-25 18:33:17 +0100

    Update plt_app.py

--- plt_app.py
+++ plt_app.py
@@ -1085,7 +1085,7 @@ if run_clicked:
             ax.set_facecolor(DARK)
             title_ax(ax, 'SHARE')
             if pos:
-                cm2 = plt.cm.get_cmap('tab20', len(pos))
+                cm2 = plt.colormaps['tab20'].resampled(len(pos))
                 wedges, texts, autos = ax.pie(
                     list(pos.values()), labels=list(pos.keys()),
                     autopct='%1.0f%%',
